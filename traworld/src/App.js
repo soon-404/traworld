75df247938262dc3cf10d591a0c93b09fc26c049
soon-404 <tfpsoon@gmail.com>
2021-08-25 22:51:09 +0700

    gridTest

---
+++ traworld/src/App.js
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className='grid grid-flow-row grid-cols-2 md:grid-cols-3 grid-rows-3 md:grid-rows-2 gap-4 w-screen h-screen'>
+      <div className='bg-red-300'>1</div>
+      <div class='bg-red-300'>2</div>
+      <div class=' bg-red-300'>3</div>
+      <div className='bg-red-300'>4</div>
+      <div class='bg-red-300'>5</div>
+      <div class=' bg-red-300'>6</div>
+    </div>
+  );
+}
+
+export default App;
