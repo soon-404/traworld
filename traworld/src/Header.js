7d95bf7d3d7847f2f177f2a835cd74fc97b0f629
soon-404 <tfpsoon@gmail.com>
2021-10-04 23:44:46 +0700

    delete sidebar.js

--- traworld/src/Header.js
+++ traworld/src/Header.js
@@ -1,12 +1,24 @@
 import React from "react";
-import Sidebar from "./Sidebar";
+import { useState } from "react";
 function Header() {
+  const [menu, setMenu] = useState(false);
+  const handleMenu = () => {
+    setMenu(!menu);
+  };
+  const styles = {
+    divClass:
+      "bg-blue-400 w-48 h-screen fixed left-0 top-14 pb-14 transition duration-500",
+  };
   return (
     <div className='bg-red-300 h-14 w-screen fixed flex flex-row items-center'>
-      <Sidebar />
+      <div
+        className={`${styles.divClass} ${
+          menu ? "transform translate-x-0" : "transform -translate-x-full"
+        }`}
+      ></div>
       <div
         className='bg-yellow-200 w-14 h-14 flex justify-center items-center cursor-pointer '
-        onClick={Sidebar.handleMenu}
+        onClick={handleMenu}
       >
         Icon
       </div>
